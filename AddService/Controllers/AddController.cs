4cbc1a0d4dc4ef3589fbf0cee1d3523131bc7889
emil dr <emil-rere@live.dk>
2023-09-29 11:15:25 +0200

    kan ikke huske hvad

--- AddService/Controllers/AddController.cs
+++ AddService/Controllers/AddController.cs
@@ -10,22 +10,20 @@ namespace AddService.Controllers;
 [Route("[controller]")]
 public class AddController : ControllerBase
 {
-    private IDbConnection historyCache = new MySqlConnection("Server=history-db;Database=history-database;Uid=historydb;Pwd=C@ch3d1v;");
 
     public AddController(){
-        historyCache.Open();
-        var tables = historyCache.Query<string>("SHOW TABLES LIKE 'historylogs'");
-        if (!tables.Any())
-        {
-            historyCache.Execute("CREATE TABLE historylogs (inputone INT NOT NULL, inputtwo INT NOT NULL, output INT NOT NULL)");
-        }
+       
     }
 
     [HttpGet]
     public long Get(long inputone, long inputtwo)
     {
-        //return num1 + num2;
-        return historyCache.QueryFirstOrDefault<int>("SELECT output FROM historylogs WHERE inputone = @inputone", new { inputone = inputone });
+        var result = inputone + inputtwo;
+
+        //Kald HistoryService API post
+
+        return result;
+        //return historyCache.QueryFirstOrDefault<int>("SELECT output FROM historylogs WHERE inputone = @inputone", new { inputone = inputone });
 
     }
 
@@ -33,6 +31,6 @@ public class AddController : ControllerBase
     public void Post([FromQuery] long inputone, [FromQuery] long inputtwo)
     {
         var output = inputone + inputtwo;
-        historyCache.Execute("REPLACE INTO historylogs (inputone, inputtwo, output) VALUES (@number, @divisors, @output)", new { inputone = inputone, inputtwo = inputtwo, output  = output });
+        //historyCache.Execute("REPLACE INTO historylogs (inputone, inputtwo, output) VALUES (@number, @divisors, @output)", new { inputone = inputone, inputtwo = inputtwo, output  = output });
     }
 }
