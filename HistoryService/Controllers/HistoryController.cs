bdbee19a12df285d8137913aa75771ac9ee06775
emil dr <emil-rere@live.dk>
2023-09-29 15:36:11 +0200

    random

--- HistoryService/Controllers/HistoryController.cs
+++ HistoryService/Controllers/HistoryController.cs
@@ -17,19 +17,24 @@ public class HistoryController : ControllerBase
         var tables = historyCache.Query<string>("SHOW TABLES LIKE 'historylogs'");
         if (!tables.Any())
         {
-            historyCache.Execute("CREATE TABLE historylogs (inputone INT NOT NULL, inputtwo INT NOT NULL, output INT NOT NULL, operation STRING NOT NULL)");
+            historyCache.Execute("CREATE TABLE historylogs (id INT NOT NULL AUTO_INCREMENT, inputone INT NOT NULL, inputtwo INT NOT NULL, output INT NOT NULL, operation VARCHAR(50) NOT NULL, PRIMARY KEY (id))");
         }
     }
 
 
     [HttpGet("/get/addition")]
-    public IEnumerable<History> GetAdditions(long inputone, long inputtwo)
+    public IEnumerable<History> GetAdditions()
     {
         //return where opretaion = addition
         //lav 2 get endpoints 1 til sub og en add.
-        var result = historyCache.QueryFirstOrDefault<History>("SELECT output FROM historylogs WHERE inputone = @inputone", new { inputone = inputone });
+        //var result = historyCache.QueryFirstOrDefault<History>("SELECT * FROM historylogs WHERE inputone = @inputone", new { inputone = inputone });
+        var result1 =  historyCache.Query<IEnumerable<History>>("SELECT inputone, inputtwo, output FROM historylogs WHERE operation = 'addition'");
+        Console.WriteLine("last: " + result1.Last());//.ToString());
+       // Console.WriteLine((IEnumerable<History>)result1.ToList());
+        foreach (var x in result1) { Console.WriteLine(x); }
         
-        return (IEnumerable<History>)result;
+        Console.WriteLine("TESTER CONSOLE");
+        return (IEnumerable<History>)result1;
 
     }
 
@@ -47,7 +52,8 @@ public class HistoryController : ControllerBase
     {
         //var output = inputone + inputtwo;
         string operation = "addition";
-        historyCache.Execute("REPLACE INTO historylogs (inputone, inputtwo, output, operation) VALUES (@number, @divisors, @output, @operation)", new { inputone = inputone, inputtwo = inputtwo, output = output, operation = operation });
+       // string id = inputone + "_" + inputtwo + "_addition";
+        historyCache.Execute("REPLACE INTO historylogs (inputone, inputtwo, output, operation) VALUES (@inputone, @inputtwo, @output, @operation)", new { inputone = inputone, inputtwo = inputtwo, output = output, operation = operation });
     }
 
     [HttpPost("/post/subtraction")]
--- HistoryService/Models/History.cs
+++ HistoryService/Models/History.cs
@@ -3,7 +3,7 @@
     public class History
     {
         public string Operation { get; set; }
-        public DateTime Timestamp { get; set; }
+     //   public DateTime Timestamp { get; set; }
         public long Num1 { get; set; }
         public long Num2 { get; set; }
         public long Result { get; set; }
