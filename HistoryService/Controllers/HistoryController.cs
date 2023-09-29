3f55fd88c61a1416f92d431f688ec0e935c41654
longhairy <londo112@hotmail.com>
2023-09-29 21:19:05 +0200

    kan se lidt nu

--- HistoryService/Controllers/HistoryController.cs
+++ HistoryService/Controllers/HistoryController.cs
@@ -28,13 +28,53 @@ public class HistoryController : ControllerBase
         //return where opretaion = addition
         //lav 2 get endpoints 1 til sub og en add.
         //var result = historyCache.QueryFirstOrDefault<History>("SELECT * FROM historylogs WHERE inputone = @inputone", new { inputone = inputone });
-        var result1 =  historyCache.Query<IEnumerable<History>>("SELECT inputone, inputtwo, output FROM historylogs WHERE operation = 'addition'");
-        Console.WriteLine("last: " + result1.Last());//.ToString());
-       // Console.WriteLine((IEnumerable<History>)result1.ToList());
-        foreach (var x in result1) { Console.WriteLine(x); }
-        
+       // var result1 =  historyCache.Query<string>("SELECT inputone, inputtwo, output FROM historylogs WHERE operation = 'addition'");
+        var result2 = historyCache.ExecuteReader("SELECT inputone, inputtwo, output FROM historylogs WHERE operation = 'addition'");
+        DataTable dataTable = new DataTable();
+
+        for (int i = 0;i<result2.FieldCount;i++)
+        {
+            DataColumn column = new DataColumn();
+            column.ColumnName=result2.GetName(i);
+            dataTable.Columns.Add(column);
+        }
+
+        while(result2.Read())
+        {
+            DataRow dataRow = dataTable.NewRow();
+            for(int i = 0; i<result2.FieldCount;i++)
+            {
+                try
+                {
+                    Console.WriteLine(result2[i].ToString());
+                    dataRow[i] = result2[i].ToString();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            dataTable.Rows.Add(dataRow);
+            dataRow = null;
+        }
+        Console.WriteLine("rows" + dataTable.Rows);
+        Console.WriteLine("cols" + dataTable.Columns);
+
+        //foreach (var x in result1) { Console.WriteLine(x); }
+        //        foreach (var item in list) { Console.WriteLine("ListItem: " + item); }
+
+        //Console.WriteLine("first: " + result1.First());
+
+        //Console.WriteLine("last: " + result1.Last());//.ToString());
+        // Console.WriteLine((IEnumerable<History>)result1.ToList());
+        //var aHistory = new History(){ Num1=long.Parse(result1.First()),Num2=long.Parse(result1.First()),Result=long.Parse(result1.First()),Operation="addition"};
+        // Console.WriteLine("Hist OBJ: "+ aHistory.Num1 +" " +aHistory.Num2 );
+
+
+
         Console.WriteLine("TESTER CONSOLE");
-        return (IEnumerable<History>)result1;
+        return (IEnumerable<History>)result2;
 
     }
 
